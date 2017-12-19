b2f53f2bd0ecaf7fcb037844dc8610b3b1c8f336
enelric <danieldunbar@email.arizona.edu>
2017-12-18 22:49:16 -0700

    Home Fragment working

--- Dunbaresume/app/src/main/java/com/dunbar/daniel/dunbaresume/MainActivity.java
+++ Dunbaresume/app/src/main/java/com/dunbar/daniel/dunbaresume/MainActivity.java
@@ -1,8 +1,13 @@
 package com.dunbar.daniel.dunbaresume;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +17,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.dunbar.daniel.dunbaresume.Fragments.HomeFragment;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, HomeFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +39,9 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        HomeFragment firstFragment = new HomeFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, firstFragment).commit();
     }
 
     @Override
@@ -65,25 +76,40 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        Fragment fragment = null;
 
         if (id == R.id.nav_home) {
-            // Handle the camera action
+            fragment = new HomeFragment();
+            Toast.makeText(this.getBaseContext(), "Home selected", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_education) {
-
+            Toast.makeText(this.getBaseContext(), "Education selected", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_work) {
-
+            Toast.makeText(this.getBaseContext(), "Work selected", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_projects) {
-
+            Toast.makeText(this.getBaseContext(), "Projects selected", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_skills) {
-
+            Toast.makeText(this.getBaseContext(), "Skills selected", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_courses) {
-
+            Toast.makeText(this.getBaseContext(), "Courses selected", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_contact) {
+            Toast.makeText(this.getBaseContext(), "Contact selected", Toast.LENGTH_SHORT).show();
+        }
 
+        if(fragment != null) {
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction transaction = fm.beginTransaction();
+            transaction.replace(R.id.fragment_container, fragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
