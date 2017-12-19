8c9c6fac21d438826bd19cff10e27fbf116db6c9
enelric <danieldunbar@email.arizona.edu>
2017-12-18 18:53:14 -0700

    Updated some UI text

--- Dunbaresume/app/src/main/java/com/dunbar/daniel/dunbaresume/MainActivity.java
+++ Dunbaresume/app/src/main/java/com/dunbar/daniel/dunbaresume/MainActivity.java
@@ -66,17 +66,19 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_education) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_work) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_projects) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_skills) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_courses) {
+
+        } else if (id == R.id.nav_contact) {
 
         }
 
